85bf9366924411238aa3ea782abc7cf8d527904e
Sattar <sattar_saud@hotmail.com>
2025-05-07 14:07:16 +0200

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Application } from "./modules/app/application";
+
+createRoot(document.getElementById("root")!).render(
+  <h1>
+    <Application />
+  </h1>,
+);
